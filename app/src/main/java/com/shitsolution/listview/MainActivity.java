9e9e42466810f61b8f57f905bd876de6523aefe1
Hridoy <hridoykrisnadas@gmail.com>
2021-03-06 17:36:21 +0600

    master

--- app/src/main/java/com/shitsolution/listview/MainActivity.java
+++ app/src/main/java/com/shitsolution/listview/MainActivity.java
@@ -2,6 +2,9 @@ package com.shitsolution.listview;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,27 +12,27 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    SearchView searchView;
-    ArrayList<String> mylist;
+    ArrayAdapter<String> arrayAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("");
 
         listView = findViewById(R.id.ListViewId);
-        searchView = findViewById(R.id.search_viewId);
 
 
         Context context;
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.sample_layout,
+        arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.sample_layout,
                 R.id.sample_Text_ViewId, getCountry());
 
         listView.setAdapter(arrayAdapter);
@@ -45,10 +48,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+
+        MenuItem menuItem = menu.findItem(R.id.searchID);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
 
                 if (getCountry().contains(query)) {
 
@@ -59,16 +73,33 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "No Match found", Toast.LENGTH_LONG).show();
 
                 }
-
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
                 return false;
             }
         });
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settingID) {
+            Toast.makeText(MainActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.feedbackID) {
+            Toast.makeText(MainActivity.this, "Feedback", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.shareID) {
+            Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public ArrayList<String> getCountry() {
